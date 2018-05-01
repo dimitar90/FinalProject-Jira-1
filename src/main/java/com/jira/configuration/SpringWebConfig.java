c1daf1ef3a6687dfbdf3cdc7b7f2fc430d4d4464
ivanlutov <ivanlutov@abv.bg>
2018-05-01 21:14:14 +0300

    modify add comment to task is AJAX query

--- src/main/java/com/jira/configuration/SpringWebConfig.java
+++ src/main/java/com/jira/configuration/SpringWebConfig.java
@@ -33,6 +33,7 @@ public class SpringWebConfig extends WebMvcConfigurerAdapter {
     	registry.addResourceHandler("/img/**").addResourceLocations("/static/images/");
         registry.addResourceHandler("/css/**").addResourceLocations("/static/css/");
         registry.addResourceHandler("/pdfs/**").addResourceLocations("/static/pdf/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/static/js/");
     }
 	
 	@Bean
